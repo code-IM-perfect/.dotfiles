05b740bb09fced9c50715379616e6745a1f647e1
Harshit Tomar <harshitt345@gmail.com>
2025-12-01 10:46:57 +0530

    fish: add starship config and set it up on fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
+    starship init fish | source
 
     bind -M "insert" ctrl-backspace "backward-kill-word"
     bind -M "insert" ctrl-delete "kill-word"
