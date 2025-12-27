3fddb8f455aba50b40d740f98daf442b6efdd00e
Harshit Tomar <harshitt345@gmail.com>
2025-12-28 01:04:06 +0530

    root: fix setup script

--- setup.sh
+++ setup.sh
@@ -7,30 +7,30 @@ read --prompt-str "Type enter to continue anyways:" hi
 echo "yo"
 
 # ln -s ~/.dotfiles/btop/btop.conf ~/.config/btop/btop.conf
-# ln -s ~/.dotfiles/cava/ ~/.config/cava/
-# ln -s ~/.dotfiles/clipse/ ~/.config/clipse/
+# ln -s ~/.dotfiles/cava/ ~/.config/cava
+# ln -s ~/.dotfiles/clipse/ ~/.config/clipse
 # ln -s ~/.dotfiles/discord/settings.json ~/.config/discord/settings.json
-# ln -s ~/.dotfiles/fish/ ~/.config/fish/
-# ln -s ~/.dotfiles/htop/ ~/.config/htop/
-# ln -s ~/.dotfiles/hypr/ ~/.config/hypr/
-# ln -s ~/.dotfiles/kitty/ ~/.config/kitty/
-# ln -s ~/.dotfiles/Kvantum/ ~/.config/Kvantum/
-# ln -s ~/.dotfiles/lazygit/ ~/.config/lazygit/
-# ln -s ~/.dotfiles/matugen/ ~/.config/matugen/
-# ln -s ~/.dotfiles/mpd/ ~/.config/mpd/
-# ln -s ~/.dotfiles/mpv/ ~/.config/mpv/
-# ln -s ~/.dotfiles/neofetch/ ~/.config/neofetch/
-# ln -s ~/.dotfiles/nicotine/ ~/.config/nicotine/
-# ln -s ~/.dotfiles/niri/ ~/.config/niri/
-# ln -s ~/.dotfiles/nvim/ ~/.config/nvim/
-# ln -s ~/.dotfiles/picom/ ~/.config/picom/
-# ln -s ~/.dotfiles/qalculate/ ~/.config/qalculate/
-# ln -s ~/.dotfiles/quickshell/ ~/.config/quickshell/
-# ln -s ~/.dotfiles/rmpc/ ~/.config/rmpc/
-# ln -s ~/.dotfiles/rofi/ ~/.config/rofi/
-# ln -s ~/.dotfiles/rsgain/ ~/.config/rsgain/
-# ln -s ~/.dotfiles/typioca/ ~/.config/typioca/
-# ln -s ~/.dotfiles/yazi/ ~/.config/yazi/
+# ln -s ~/.dotfiles/fish/ ~/.config/fish
+# ln -s ~/.dotfiles/htop/ ~/.config/htop
+# ln -s ~/.dotfiles/hypr/ ~/.config/hypr
+# ln -s ~/.dotfiles/kitty/ ~/.config/kitty
+# ln -s ~/.dotfiles/Kvantum/ ~/.config/Kvantum
+# ln -s ~/.dotfiles/lazygit/ ~/.config/lazygit
+# ln -s ~/.dotfiles/matugen/ ~/.config/matugen
+# ln -s ~/.dotfiles/mpd/ ~/.config/mpd
+# ln -s ~/.dotfiles/mpv/ ~/.config/mpv
+# ln -s ~/.dotfiles/neofetch/ ~/.config/neofetch
+# ln -s ~/.dotfiles/nicotine/ ~/.config/nicotine
+# ln -s ~/.dotfiles/niri/ ~/.config/niri
+# ln -s ~/.dotfiles/nvim/ ~/.config/nvim
+# ln -s ~/.dotfiles/picom/ ~/.config/picom
+# ln -s ~/.dotfiles/qalculate/ ~/.config/qalculate
+# ln -s ~/.dotfiles/quickshell/ ~/.config/quickshell
+# ln -s ~/.dotfiles/rmpc/ ~/.config/rmpc
+# ln -s ~/.dotfiles/rofi/ ~/.config/rofi
+# ln -s ~/.dotfiles/rsgain/ ~/.config/rsgain
+# ln -s ~/.dotfiles/typioca/ ~/.config/typioca
+# ln -s ~/.dotfiles/yazi/ ~/.config/yazi
 #
 # # TODO: add files in .conf
 # ln -s ~/.dotfiles/.clang-format ~/.clang-format
