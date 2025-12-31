b3a7cb9142546148a1b6a14abeb4e51e6432542c
Harshit Tomar <harshitt345@gmail.com>
2026-01-01 00:03:09 +0530

    nvim: update lspconfig syntax

--- nvim/init.lua
+++ nvim/init.lua
@@ -900,6 +900,7 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-require('lspconfig').qmlls.setup {
+vim.lsp.config('qmlls', {
   cmd = { 'qmlls6' },
-}
+})
+vim.lsp.enable { 'ts_ls' }
