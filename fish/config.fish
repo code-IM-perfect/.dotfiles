1e66ce72b67ea6aef0322ae4119cc3f8c68cd01b
Harshit Tomar <harshitt345@gmail.com>
2026-01-02 01:39:39 +0530

    fish: add reminder comment

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
     set -gx STARSHIP_CONFIG ~/.config/fish/starship.toml
     starship init fish | source
 
+    # Remember to enable the ssh-agent service before, cuz like otherwise this socket won't be created
     set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 
     bind -M "insert" ctrl-backspace "backward-kill-word"
