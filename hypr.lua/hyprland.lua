8fe3d082f91f4dcb5ea11b3c91e03b5814cea6fc
Harshit Tomar <harshitt345@gmail.com>
2026-05-15 13:04:18 +0530

    hypr.lua: add permissions config
    Signed-off-by: Harshit Tomar <harshitt345@gmail.com>

--- hypr.lua/hyprland.lua
+++ hypr.lua/hyprland.lua
@@ -76,3 +76,21 @@ hl.env("HYPRCURSOR_SIZE", "32")
 
 require("core.env")
 
+-----------------------
+----- PERMISSIONS -----
+-----------------------
+
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Permissions/
+-- Please note permission changes here require a Hyprland restart and are not applied on-the-fly
+-- for security reasons
+
+-- hl.config({
+--   ecosystem = {
+--     enforce_permissions = true,
+--   },
+-- })
+
+-- hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
+-- hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
+-- hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
+
