c63d310044f8d73af375bceeab2094a13a8e5953
Harshit Tomar <harshitt345@gmail.com>
2026-05-15 11:04:18 +0530

    hypr.lua: add monitor config
    Signed-off-by: Harshit Tomar <harshitt345@gmail.com>

--- hypr.lua/hyprland.lua
+++ hypr.lua/hyprland.lua
@@ -8,3 +8,43 @@
 -- You can (and should!!) split this configuration into multiple files
 -- Create your files separately and then require them like this:
 -- require("myColors")
+
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+	output = "",
+	mode = "preferred",
+	position = "auto",
+	scale = "auto",
+})
+
+hl.monitor({
+	output = "DP-1",
+	mode = "highrr",
+	position = "auto-right",
+	scale = 1,
+})
+
+hl.monitor({
+	output = "DP-2",
+	mode = "highrr",
+	position = "auto-right",
+	scale = 1,
+})
+
+hl.monitor({
+	output = "HDMI-A-1",
+	mode = "preferred",
+	position = "auto-left",
+	scale = 1,
+})
+
+hl.monitor({
+	output = "eDP-1",
+	mode = "preferred",
+	position = "auto-left",
+	scale = 1,
+})
