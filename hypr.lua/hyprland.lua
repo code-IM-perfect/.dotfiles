de7b47e9ec2036d80a28807f484791c8ab4b7e58
Harshit Tomar <harshitt345@gmail.com>
2026-05-18 23:05:50 +0530

    hypr.lua: add dwindle layout config

--- hypr.lua/hyprland.lua
+++ hypr.lua/hyprland.lua
@@ -164,6 +164,13 @@ require("core.animations")
 --     rounding    = 0,
 -- })
 
+-- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
+hl.config({
+	dwindle = {
+		preserve_split = true, -- You probably want this
+	},
+})
+
 ---------------------
 ---- KEYBINDINGS ----
 ---------------------
