cdb63ef94de51a45436fb2358724953875b158dc
Harshit Tomar <harshitt345@gmail.com>
2026-05-18 23:06:15 +0530

    hypr.lua: add master layout config

--- hypr.lua/hyprland.lua
+++ hypr.lua/hyprland.lua
@@ -171,6 +171,13 @@ hl.config({
 	},
 })
 
+-- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
+hl.config({
+	master = {
+		new_status = "master",
+	},
+})
+
 ---------------------
 ---- KEYBINDINGS ----
 ---------------------
