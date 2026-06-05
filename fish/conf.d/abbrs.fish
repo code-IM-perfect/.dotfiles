8c2c87382717654d69be7bfb9aa434294f4b8ce3
Harshit Tomar <harshitt345@gmail.com>
2026-06-05 16:09:24 +0530

    fish: add git commit with auto date abbr

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -1,4 +1,8 @@
+function __abbr_git_commit_date
+    echo "git commit --date='$(date +"%Y-%m-%dT%H:%M:%S")' -sm \"%\""
+end
 
 abbr --add gcm --set-cursor "git commit -sm \"%\""
+abbr --add gcmd --set-cursor --function __abbr_git_commit_date
 abbr --add gpull "git pull"
 abbr --add gpush "git push"
