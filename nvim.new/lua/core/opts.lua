997883bddc557bf07eb04358b416f00597655dc0
Harshit Tomar <harshitt345@gmail.com>
2026-06-18 12:37:32 +0530

    nvim.new: configure line numbers

--- nvim.new/lua/core/opts.lua
+++ nvim.new/lua/core/opts.lua
@@ -5,3 +5,7 @@ vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
+
+-- Interface
+vim.opt.number = true
+vim.opt.relativenumber = true
