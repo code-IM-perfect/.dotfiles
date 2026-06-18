61e522bda9de2f26e6596500dd8f9aeeddb08850
Harshit Tomar <harshitt345@gmail.com>
2026-06-18 14:02:57 +0530

    nvim.new: enable highlighting on search and add binding to disable it

--- nvim.new/lua/core/opts.lua
+++ nvim.new/lua/core/opts.lua
@@ -23,3 +23,7 @@ vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
