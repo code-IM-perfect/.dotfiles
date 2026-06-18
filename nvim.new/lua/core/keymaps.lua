0b1077be562cc30ad2a456088cef9a56fc76a867
Harshit Tomar <harshitt345@gmail.com>
2026-06-19 05:17:59 +0530

    nvim.new: add keymaps for yanking to clipboard

--- nvim.new/lua/core/keymaps.lua
+++ nvim.new/lua/core/keymaps.lua
@@ -7,3 +7,8 @@ vim.keymap.set("v", "<space>x", ":lua<CR>")
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+-- Yank to system clipboard
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+
