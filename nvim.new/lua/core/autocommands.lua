90ab0e6f5fc644ab8875b9e75a8270b157475d6b
Harshit Tomar <harshitt345@gmail.com>
2026-06-19 05:23:05 +0530

    nvim.new: add autocommand for highlighting yanked text

--- nvim.new/lua/core/autocommands.lua
+++ nvim.new/lua/core/autocommands.lua
@@ -1,1 +1,8 @@
-
+-- Highlight while yanking
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
