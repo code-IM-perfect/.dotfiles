8e4dc5732a6f1cd161f34b2f5132e86d967364f4
Harshit Tomar <harshitt345@gmail.com>
2026-06-19 05:13:46 +0530

    hypr: rearrange autostart list

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -8,8 +8,8 @@
 -- end)
 
 hl.on("hyprland.start", function()
-	hl.exec_cmd("hypridle")
 	hl.exec_cmd("quickshell")
+	hl.exec_cmd("hypridle")
 	hl.exec_cmd("systemctl --user start hyprpolkitagent")
 	hl.exec_cmd("awww-daemon")
 	hl.exec_cmd("playerctld daemon")
