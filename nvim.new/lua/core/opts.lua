cc0ef0c33ee5ac77384c1ab090e6e64d2f73daba
Harshit Tomar <harshitt345@gmail.com>
2026-06-18 14:01:55 +0530

    nvim.new: make search case-insensitive by default

--- nvim.new/lua/core/opts.lua
+++ nvim.new/lua/core/opts.lua
@@ -19,3 +19,7 @@ vim.opt.mouse = 'nv'
 -- Save undo history
 vim.opt.undofile = true
 
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
