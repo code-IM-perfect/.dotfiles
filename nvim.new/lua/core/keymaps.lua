d9a9f5369e68d255cb9b55c0868498c8e4d0d932
Harshit Tomar <harshitt345@gmail.com>
2026-06-19 05:16:47 +0530

    nvim.new: add keymaps for quick testing of snippets

--- nvim.new/lua/core/keymaps.lua
+++ nvim.new/lua/core/keymaps.lua
@@ -1,1 +1,5 @@
+-- Quick testing (nvim/lua only tho)
+vim.keymap.set("n", "<space><space>re", "<cmd>restart<CR>")
+vim.keymap.set("n", "<space>x", ".lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
 
