6ba41f1f41276325214675c1f36b09e135d27136
Harshit Tomar <harshitt345@gmail.com>
2026-06-19 05:15:04 +0530

    fish: add abbr for git clone with depth 1

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -6,3 +6,4 @@ abbr --add gcm --set-cursor "git commit -sm \"%\""
 abbr --add gcmd --set-cursor --function __abbr_git_commit_date
 abbr --add gpull "git pull"
 abbr --add gpush "git push"
+abbr --add gcloned "git clone --depth=1"
