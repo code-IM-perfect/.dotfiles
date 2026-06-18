f9b791445d317d22369f549fedb0bd0e54936217
Harshit Tomar <harshitt345@gmail.com>
2026-06-19 05:19:53 +0530

    nvim.new: add keymaps for switching between windows

--- nvim.new/lua/core/keymaps.lua
+++ nvim.new/lua/core/keymaps.lua
@@ -3,6 +3,13 @@ vim.keymap.set("n", "<space><space>re", "<cmd>restart<CR>")
 vim.keymap.set("n", "<space>x", ".lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
 -- move selected text regions
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
