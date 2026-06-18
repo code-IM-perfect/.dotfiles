7ddcb74c59e9cca56add509d2392ffd11f40b052
Harshit Tomar <harshitt345@gmail.com>
2026-06-19 05:17:42 +0530

    nvim.new: add keymaps for moving selected text range

--- nvim.new/lua/core/keymaps.lua
+++ nvim.new/lua/core/keymaps.lua
@@ -3,3 +3,7 @@ vim.keymap.set("n", "<space><space>re", "<cmd>restart<CR>")
 vim.keymap.set("n", "<space>x", ".lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+-- move selected text regions
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
