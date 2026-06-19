fa503b773dd9095830409ffeb8636a7302c6f275
Harshit Tomar <harshitt345@gmail.com>
2026-06-19 05:54:12 +0530

    nvim.new: add autocmd to run TSUpdate after nvim-treesitter is updated

--- nvim.new/lua/extra/vim_pack.lua
+++ nvim.new/lua/extra/vim_pack.lua
@@ -4,3 +4,19 @@ end
 
 require("extra.plugins.theme.tokyonight")
 require("extra.plugins.core.treesitter")
+
+do
+  vim.api.nvim_create_autocmd('PackChanged', {
+    callback = function(ev)
+      local name = ev.data.spec.name
+      local kind = ev.data.kind
+      if kind ~= 'install' and kind ~= 'update' then return end
+
+      if name == 'nvim-treesitter' then
+        if not ev.data.active then vim.cmd.packadd 'nvim-treesitter' end
+        vim.cmd 'TSUpdate'
+        return
+      end
+    end,
+  })
+end
