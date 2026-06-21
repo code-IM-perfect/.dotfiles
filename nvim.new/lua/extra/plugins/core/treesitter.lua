46b1fe3efddaa86caf25e4fae89d7662eb5a91b3
Harshit Tomar <harshitt345@gmail.com>
2026-06-21 12:18:40 +0530

    nvim.new: add some default parsers to nvim-treesitter

--- nvim.new/lua/extra/plugins/core/treesitter.lua
+++ nvim.new/lua/extra/plugins/core/treesitter.lua
@@ -1,1 +1,5 @@
 vim.pack.add { { src = _G.gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
+
+local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+
+require('nvim-treesitter').install(parsers)
