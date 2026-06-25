63428d2f2edcca22b4ab1a13f606e1c55e8c9f36
Harshit Tomar <harshitt345@gmail.com>
2026-06-25 14:45:48 +0530

    nvim.new: add mini.statusline

--- nvim.new/lua/core/vim_pack.lua
+++ nvim.new/lua/core/vim_pack.lua
@@ -7,6 +7,8 @@ require 'plugins.core.treesitter'
 
 vim.pack.add { _G.gh 'nvim-mini/mini.nvim' }
 
+require 'plugins.interface.mini_statusline'
+
 require 'plugins.editor.mini_ai'
 require 'plugins.editor.mini_pairs'
 
