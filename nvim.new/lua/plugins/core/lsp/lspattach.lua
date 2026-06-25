aea4291ecba935fb403eb7eb18b1cfa68ec9e527
Harshit Tomar <harshitt345@gmail.com>
2026-06-25 19:50:09 +0530

    nvim.new: add autocommands to highlight references on cursor-hold

--- nvim.new/lua/plugins/core/lsp/lspattach.lua
+++ nvim.new/lua/plugins/core/lsp/lspattach.lua
@@ -2,7 +2,6 @@
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('when-lsp-attach', { clear = true }),
   callback = function(event)
-
     -- helper func to define keybinds
     local map = function(keys, func, desc, mode)
       mode = mode or 'n'
@@ -18,4 +17,33 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Execute a code action, usually your cursor needs to be on top of an error
     -- or a suggestion from your LSP for this to activate.
     map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+    -- Highlight references of current word (See `:help CursorHold`)
+    if client and client:supports_method('textDocument/documentHighlight', event.buf) then
+      local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight-onHold', { clear = false })
+
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      -- Remove highlight on cursor move :P
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      -- Remove the highlighting autocommands on detach
+      vim.api.nvim_create_autocmd('LspDetach', {
+        group = vim.api.nvim_create_augroup('when-lsp-detach', { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds { group = 'lsp-highlight-onHold', buffer = event2.buf }
+        end,
+      })
+    end
 })
