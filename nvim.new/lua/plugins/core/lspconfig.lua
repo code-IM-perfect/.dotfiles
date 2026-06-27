0e7c7e5f6a6eb9ddca11deffe2e727c281456a5c
Harshit Tomar <harshitt345@gmail.com>
2026-06-27 18:22:21 +0530

    nvim.new: rearrange call priority

--- nvim.new/lua/plugins/core/lspconfig.lua
+++ nvim.new/lua/plugins/core/lspconfig.lua
@@ -1,9 +1,9 @@
 vim.pack.add { { src = _G.gh 'neovim/nvim-lspconfig' } }
 
--- Stuff to set up on LspAttach (some keybinds and highlight-on-hold [for now :P])
-require 'plugins.core.lsp.lspattach'
 -- Progress info for LSP stuff
 require 'plugins.core.lsp.fidget'
+-- Stuff to set up on LspAttach (some keybinds and highlight-on-hold [for now :P])
+require 'plugins.core.lsp.lspattach'
 -- Community maintained recommended setup for lua_ls (so I don't have to)
 require 'plugins.core.lsp.lazydev'
 -- Formatting
