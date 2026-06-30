29507147a39b37531359fcc15a9b9c77cf3344a3
Harshit Tomar <harshitt345@gmail.com>
2026-06-30 13:19:46 +0530

    nvim.new: configure how new splits should open

--- nvim.new/lua/core/opts.lua
+++ nvim.new/lua/core/opts.lua
@@ -29,6 +29,10 @@ vim.o.signcolumn = 'yes'
 -- Decrease update time
 vim.o.updatetime = 250
 
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
